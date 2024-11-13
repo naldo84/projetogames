23e43fe2e7f1d8ad4abe6db6142fd285b5f55703
Erinaldo <erinaldo2005@gmail.com>
2024-11-13 20:57:03 -0300

    Inclusão do GET gameById

--- src/main/java/com/projetogames/projetogames/controllers/GameController.java
+++ src/main/java/com/projetogames/projetogames/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.projetogames.projetogames.controllers;
 
+import com.projetogames.projetogames.dto.GameDTO;
 import com.projetogames.projetogames.dto.GameMinDTO;
 import com.projetogames.projetogames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping (value = "/{id}") //Essa anotação indica que o valor do {id} na URL será mapeado para o parâmetro id do métodoh
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/projetogames/projetogames/services/GameService.java
+++ src/main/java/com/projetogames/projetogames/services/GameService.java
@@ -1,9 +1,12 @@
 package com.projetogames.projetogames.services;
 
+import com.projetogames.projetogames.dto.GameDTO;
 import com.projetogames.projetogames.dto.GameMinDTO;
+import com.projetogames.projetogames.entities.Game;
 import com.projetogames.projetogames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +16,25 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+/*    Uma boa prática é usar em tohdo o service o @Transactinal (Spring), pois garante que a operação
+    do banco de dados aconteça obedecende o ACID.
+    */
+
+
+    @Transactional(readOnly = true) //garante no BD que a trasnação não fará operaçaõ de escrita (ficando mais rápido)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get(); //O findById retorna um optinal. Nesse caso é necessário incluir o .get.
+
+        return new GameDTO(result); //transformando o objeto game em GAMEDTO (uso do construtor)
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         List<GameMinDTO> gameMinDto = result.stream().map(gameDto -> new GameMinDTO(gameDto)).toList();
+
         return gameMinDto;
     }
+
+
 }
