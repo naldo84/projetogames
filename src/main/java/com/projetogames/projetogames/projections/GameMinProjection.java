9ab56a9bf88bc8f3174a678e0c377dcce74ce1dd
Erinaldo <erinaldo2005@gmail.com>
2024-11-20 01:11:06 -0300

    Homolog OK

--- src/main/java/com/projetogames/projetogames/projections/GameMinProjection.java
+++ src/main/java/com/projetogames/projetogames/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.projetogames.projetogames.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
